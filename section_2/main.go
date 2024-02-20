8966008ef35aa50ae223d87e865930915588af26
João Felipe Zini <joaofelipezini@gmail.com>
2024-02-20 10:02:53 -0300

    interface and switch/case

--- section_2/main.go
+++ section_2/main.go
@@ -6,10 +6,17 @@ import "fmt"
 
 func main() {
 	// this type of declaration in only valid inside a function
-	cards := []string{"Ace of Diamonds", newCard()}
+	cards := []interface{}{"Ace of Diamonds", newCard(), 15, 29}
 	cards = append(cards, "Six of Spades")
-	for i, card := range cards {
-		fmt.Println(i, card)
+	for _, card := range cards {
+		switch v := card.(type) {
+		case string:
+			fmt.Println("String: ", v)
+		case int:
+			fmt.Println("Int: ", v)
+		default:
+			fmt.Println("Unknown type")
+		}
 	}
 	fmt.Println(cards)
 }
